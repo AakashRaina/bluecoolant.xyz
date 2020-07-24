caee0a7b83bf647ab039548f904304fb55e51cfe
AakashRaina <aakashraina9@gmail.com>
2020-07-24 21:24:00 +0530

    [Fonts] Adds google fonts 🎉

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,10 +11,10 @@ const IndexPage = () => (
   <div className='container mx-auto'>
     <SEO title="Home" />
     <div className='h-35 pl-5 pt-5 border-b-2 border-gray-400'>
-      <div className='text-4xl font-bold'>
+      <div className='text-4xl font-bold subpixel-antialiased'>
         Aakash Raina
       </div>
-      <div className='pt-8 space-x-4'>
+      <div className='pt-5 pb-3 space-x-4'>
         <span><FaTwitter color='#5f6c80' /></span>
         <span><FaLinkedin color='#5f6c80' /></span>
         <span><FiMail color='#5f6c80' /></span>

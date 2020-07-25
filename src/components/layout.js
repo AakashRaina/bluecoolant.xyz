d6da30f8f546c09b190d7e3b56d7b6e154c9d734
AakashRaina <aakashraina9@gmail.com>
2020-07-25 13:08:44 +0530

    [Refactor] Adds layout component ♻️

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,51 +1,42 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from 'react';
-import PropTypes from 'prop-types';
-import { useStaticQuery, graphql } from 'gatsby';
-
-import Header from './header';
-// import "./layout.css"
+import '../css/index.css';
+import { FaTwitter, FaLinkedin, FaGithub } from 'react-icons/fa';
+import { FiMail } from 'react-icons/fi';
+import Helmet from 'react-helmet';
+import useSiteMetadata from '../hooks/useSiteMetadata';
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
+  const { title, description } = useSiteMetadata();
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+    <div className="container mx-auto">
+      <Helmet>
+        <html lang="en" />
+        <title>{title}</title>
+        <meta name="desc" content={description} />
+      </Helmet>
+      <div className="h-35 pl-5 pt-5 border-b-2 border-gray-300">
+        <div className="text-blue-500 text-5xl font-bold subpixel-antialiased">
+          Aakash
+        </div>
+        <div className="pt-5 pb-3 space-x-4">
+          <span>
+            <FaTwitter color="#5f6c80" />
+          </span>
+          <span>
+            <FaLinkedin color="#5f6c80" />
+          </span>
+          <span>
+            <FaGithub color="#5f6c80" />
+          </span>
+          <span>
+            <FiMail color="#5f6c80" />
+          </span>
+        </div>
       </div>
-    </>
+      {children}
+    </div>
   );
-};
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 };
 
 export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,9 @@
 import React from 'react';
-import '../css/index.css';
-import { FaTwitter, FaLinkedin, FaGithub } from 'react-icons/fa';
-import { FiMail } from 'react-icons/fi';
-import Helmet from 'react-helmet';
-import useSiteMetadata from '../hooks/useSiteMetadata';
+import Layout from '../components/layout';
 
 const IndexPage = () => {
-  const { title, description } = useSiteMetadata();
-
   return (
-    <div className="container mx-auto">
-      <Helmet>
-        <html lang="en" />
-        <title>{title}</title>
-        <meta name="desc" content={description} />
-      </Helmet>
-      <div className="h-35 pl-5 pt-5 border-b-2 border-gray-300">
-        <div className="text-blue-500 text-5xl font-bold subpixel-antialiased">
-          Aakash
-        </div>
-        <div className="pt-5 pb-3 space-x-4">
-          <span>
-            <FaTwitter color="#5f6c80" />
-          </span>
-          <span>
-            <FaLinkedin color="#5f6c80" />
-          </span>
-          <span>
-            <FaGithub color="#5f6c80" />
-          </span>
-          <span>
-            <FiMail color="#5f6c80" />
-          </span>
-        </div>
-      </div>
+    <Layout>
       <div className="h-64 pl-5 pr-5 pt-20 align-middle subpixel-antialiased">
         <div className="text-4xl text-blue-500 text-5xl">Hi.</div>
         <div className="text-gray-600 bio text-2xl">
@@ -43,10 +13,11 @@ const IndexPage = () => {
           <p className="text-blue-500">Javascript</p>,{' '}
           <p className="text-blue-500">React</p>,{' '}
           <p className="text-blue-500">Web performance</p> 🚀 and{' '}
-          <p className="text-blue-500">Frontend</p> development in general.❤️ anything Web.
+          <p className="text-blue-500">Frontend</p> development in general.❤️
+          anything Web.
         </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 

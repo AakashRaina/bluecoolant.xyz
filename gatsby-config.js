ab34bfdd3066c9f4d10ef778e88b021ed4daf733
AakashRaina <aakashraina9@gmail.com>
2020-07-26 16:17:34 +0530

    [Web manifest] Updates manifest display property

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `Personal Blog`,
+        display: `minimal-ui`,
         icon: `src/images/Aakash.png`,
       },
     },

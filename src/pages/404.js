b2ece63068f71a811ec0f4ded7cbb03c8c3a9459
AakashRaina <aakashraina9@gmail.com>
2020-07-29 20:46:11 +0530

    Adds 404 page ✨

---
+++ src/pages/404.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Layout from '../components/layout';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+
+export const imageQuery = graphql`
+  query MyQuery {
+    file(relativePath: { eq: "404.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
+
+const PageNotFound = ({ data }) => {
+  return (
+    <Layout>
+      <Img alt='' fluid={data.file.childImageSharp.fluid} />
+    </Layout>
+  );
+};
+
+export default PageNotFound;

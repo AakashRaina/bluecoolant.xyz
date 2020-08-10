7638b4316f380875207cf707fb45d139c5eec342
AakashRaina <aakashraina9@gmail.com>
2020-08-10 20:06:29 +0530

    [SEO] Improves SEO component

--- src/components/seo.js
+++ src/components/seo.js
@@ -6,7 +6,13 @@ const Seo = ({ title, description }) => {
     <Helmet>
       <html lang="en" />
       <title>{title}</title>
-      <meta name="desc" content={description} />
+      <meta name="description" content={description} />
+      {/* Open graph */}
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      {/* Twitter */}
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
     </Helmet>
   );
 };

767f88c970b5428e2b0a43d8ffc3b45ae42b85c1
AakashRaina <aakashraina9@gmail.com>
2020-08-24 23:55:43 +0530

    Refactored skills component

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,39 +1,33 @@
 import React, { useState } from 'react';
 import '../css/skills.styl';
 
-const data = {
-  Languages: ['Javascript', 'HTML5/CSS3', 'GraphQL', 'Java', 'Python'],
-  'Frameworks/Runtimes': ['Nodejs', 'Expressjs', 'Gatsbyjs', 'React Native'],
-  'Libraries/Tools': [
-    'Reactjs',
-    'Redux',
-    'Redux-Thunk',
-    'Webpack',
-    'Storybookjs',
-    'Tailwind',
-  ],
-};
+const SKILLS = [
+  'Javascript',
+  'HTML5/CSS3',
+  'GraphQL',
+  'Java',
+  'Python',
+  'Nodejs',
+  'Expressjs',
+  'Gatsbyjs',
+  'React Native',
+  'Reactjs',
+  'Redux',
+  'Redux-Thunk',
+  'Webpack',
+  'Storybookjs',
+  'Tailwind',
+];
 
 const Skills = _ => {
-  const [domain, setDomain] = useState('Languages');
-
-  const handleChange = e => setDomain(e.target.value);
-
   return (
     <div className="skills">
       <div>
-        <select onChange={handleChange}>
-          <option>Languages</option>
-          <option>Frameworks/Runtimes</option>
-          <option>Libraries/Tools</option>
-        </select>
-      </div>
-      <div>
         <span>
-          {data[domain].map((item, idx) => {
+          {SKILLS.map((skill, idx) => {
             return (
               <p class="skill-item" key={idx}>
-                {item}
+                {skill}
               </p>
             );
           })}

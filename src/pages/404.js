5da159bb384d88a646759d7925c0798663ffa372
AakashRaina <aakashraina9@gmail.com>
2020-12-24 23:18:22 +0530

    Updated 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from '../components/layout';
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
+import '../css/404.styl';
 
 export const imageQuery = graphql`
   query {
@@ -22,7 +23,17 @@ export const imageQuery = graphql`
 const PageNotFound = ({ data }) => {
   return (
     <Layout>
-      <Img alt="" fluid={data.file.childImageSharp.fluid} />
+      <div className="page-not-found">
+        <Img alt="" fluid={data.file.childImageSharp.fluid} />
+      </div>
+      <div className="page-not-found-content">
+        <span>
+          Hey!! You found something. Let me ask you a question. Are YOU aware of
+          Time or is Time aware of YOU? It's the former isn't it? Time exists in
+          YOU. Infact, everything exists in YOU ⚡️ Didn't get it? Google
+          <p>Advaita Vedanta</p> 😉
+        </span>
+      </div>
     </Layout>
   );
 };

c30da812ecfd205360cfc8bcf506c6965c7afedc
AakashRaina <aakashraina9@gmail.com>
2020-12-25 16:14:15 +0530

    Added social links to profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import Img from 'gatsby-image';
 import useProfileImage from '../hooks/useProfileImage';
+import { FaTwitter, FaLinkedin, FaGithub, FaFilePdf } from 'react-icons/fa';
+import { FiMail } from 'react-icons/fi';
+import useSiteMetadata from '../hooks/useSiteMetadata';
 import '../css/profile.styl';
 
-const Profile = props => {
+const Profile = _ => {
   const data = useProfileImage();
+  const { github, twitter, linkedIn, mailto, resume } = useSiteMetadata();
   return (
     <div className="profile-container">
       <div className="profile-image">
         <Img className="image" fluid={data.file.childImageSharp.fluid} />
       </div>
-      <div className="social-profiles">Icons here</div>
+      <div className="social-profiles">
+        <span>
+          <a href={twitter} target="_blank" rel="noreferrer">
+            <FaTwitter color="var(--secondary-color)" />
+          </a>
+        </span>
+        <span>
+          <a href={linkedIn} target="_blank" rel="noreferrer">
+            <FaLinkedin color="var(--secondary-color)" />
+          </a>
+        </span>
+        <span>
+          <a href={github} target="_blank" rel="noreferrer">
+            <FaGithub color="var(--secondary-color)" />
+          </a>
+        </span>
+        <span>
+          <a href={mailto}>
+            <FiMail color="var(--secondary-color)" />
+          </a>
+        </span>
+        <span>
+          <a href={resume} target="_blank" rel="noreferrer">
+            <FaFilePdf color="var(--secondary-color)" />
+          </a>
+        </span>
+      </div>
     </div>
   );
 };
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { FaTwitter, FaLinkedin, FaGithub } from 'react-icons/fa';
-import { FiMail } from 'react-icons/fi';
-import { RiFileLine } from 'react-icons/ri';
-import useSiteMetadata from '../hooks/useSiteMetadata';
 import { defineCustomElements as deckDeckGoHighlightElement } from '@deckdeckgo/highlight-code/dist/loader';
 import { Link } from 'gatsby';
 import { ThemeToggler } from 'gatsby-plugin-dark-mode';
 import '../css/layout.styl';
 
 const Layout = ({ children }) => {
-  const { github, twitter, linkedIn, mailto, resume } = useSiteMetadata();
   deckDeckGoHighlightElement();
 
   return (
@@ -24,31 +19,7 @@ const Layout = ({ children }) => {
                 </Link>
               </div>
               <div className="social-links">
-                <span>
-                  <a href={twitter} target="_blank" rel="noreferrer">
-                    <FaTwitter color="var(--secondary-color)" />
-                  </a>
-                </span>
-                <span>
-                  <a href={linkedIn} target="_blank" rel="noreferrer">
-                    <FaLinkedin color="var(--secondary-color)" />
-                  </a>
-                </span>
-                <span>
-                  <a href={github} target="_blank" rel="noreferrer">
-                    <FaGithub color="var(--secondary-color)" />
-                  </a>
-                </span>
-                <span>
-                  <a href={mailto}>
-                    <FiMail color="var(--secondary-color)" />
-                  </a>
-                </span>
-                <span>
-                  <a href={resume} target="_blank" rel="noreferrer">
-                    <RiFileLine color="var(--secondary-color)" />
-                  </a>
-                </span>
+                <span />
               </div>
               <span className="blog-section">
                 <Link to="/blog" className="highlight-text">
